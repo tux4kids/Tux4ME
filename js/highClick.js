c6fffa5c77dd1374c9e48353c951e7f7babf16df
Vignesh S <svignesh0308@gmail.com>
2015-06-22 20:17:20 +0530

    Error corrections

--- js/highClick.js
+++ js/highClick.js
@@ -7,7 +7,7 @@ var game = new Phaser.Game(640, 520, Phaser.AUTO, '', { preload: preload, create
 WebFontConfig = {
     //  'active' means all requested fonts have finished loading
     //  We set a 1 second delay before calling 'createText'.
-    active: function() { game.time.events.add(Phaser.Timer.SECOND, create, this); },
+    active: function() { game.time.events.add(Phaser.Timer.SECOND, createText, this); },
     //  The Google Fonts we want to load.
     google: { families: [ 'Architects+Daughter::latin' ] }
 };
